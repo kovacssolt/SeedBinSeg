5de1a358202ddf56466c44a6cd80a7c4d4f72c26
kovacssolt <56399961+kovacssolt@users.noreply.github.com>
2020-02-03 02:18:37 +0100

    Update SeedBS.R

--- SeedBS.R
+++ SeedBS.R
@@ -40,31 +40,31 @@ library(wbs)
 ##########################################################################################################################
 
 seeded.intervals <- function(n, decay = sqrt(2), unique.int = F){
-    n <- as.integer(n)
+	n	<- as.integer(n)
+	depth	<- log(n, base = decay)
+	depth	<- ceiling(depth)
+	M	<- sum(2^(1:depth)-1)
+	
+	boundary_mtx           <- matrix(NA, ncol = 2)
+	colnames(boundary_mtx) <- c("st", "end")
+	boundary_mtx[1, ]      <- c(1, n)
 
-	  depth	<- log(n, base = decay)
-	  depth	<- ceiling(depth)
-	  M     <- sum(2^(1:depth)-1)
-
-    boundary_mtx           <- matrix(NA, ncol = 2)
-    colnames(boundary_mtx) <- c("st", "end")
-    boundary_mtx[1, ]      <- c(1, n)
-
-		depth	<- log(n, base = decay)
-		depth	<- ceiling(depth)
+	depth	<- log(n, base = decay)
+	depth	<- ceiling(depth)
 
 
     for(i in 2:depth){
-	    int_length	<- n * (1/decay)^(i-1)
-	
-	    n_int		<- ceiling(round(n/int_length, 14))*2-1		# sometimes very slight numerical inaccuracies
-
-	    boundary_mtx	<- rbind(boundary_mtx,
+		int_length	<- n * (1/decay)^(i-1)
+		
+	    	n_int		<- ceiling(round(n/int_length, 14))*2-1		# sometimes very slight numerical inaccuracies
+	    	
+	    	boundary_mtx	<- rbind(boundary_mtx,
 			    			cbind(floor(seq(1, n-int_length, length.out = (n_int))), 
-						    		ceiling(seq(int_length, n, length.out = (n_int)))))
+						    	ceiling(seq(int_length, n, length.out = (n_int)))))
     }
-    if(unique.int){return(unique(boundary_mtx))}
-    boundary_mtx
+
+	if(unique.int){return(unique(boundary_mtx))}
+	boundary_mtx
 }
 
 ##########################################################################################################################
@@ -87,17 +87,17 @@ random.intervals <- function (n, M, unique.int = F){
     n <- as.integer(n)
     M <- as.integer(M)
     intervals <- matrix(0, nrow = M, ncol = 2)
-    intervals[, 1] <- floor(runif(M, min = 1, max = n))		  	# intended starting point
-    intervals[, 2] <- ceiling(runif(M, min = 1, max = n))		  # intended end point
+    intervals[, 1] <- floor(runif(M, min = 1, max = n))		# intended starting point
+    intervals[, 2] <- ceiling(runif(M, min = 1, max = n))	# intended end point
     for (i in 1:M){
     	tmp <- intervals[i, ] 
-    	while(tmp[1]==tmp[2]){	                                # in case the two happen to be equal, take new ones.
+    	while(tmp[1]==tmp[2]){	                                # in case the two happen to be equal, take new ones
     		
-    							tmp[1] <- floor(runif(1, min = 1, max = n)) 
-    							tmp[2] <- ceiling(runif(1, min = 1, max = n))
-    							intervals[i, ] <- tmp
+    		tmp[1] <- floor(runif(1, min = 1, max = n)) 
+    		tmp[2] <- ceiling(runif(1, min = 1, max = n))
+    		intervals[i, ] <- tmp
     	}
-    	if(tmp[1] > tmp[2]){intervals[i, ] <- tmp[2:1]}    	  	# make sure they are ordered
+    	if(tmp[1] > tmp[2]){intervals[i, ] <- tmp[2:1]}    	# make sure they are ordered
     }
     if(unique.int){return(unique(intervals))}
     intervals
@@ -123,7 +123,7 @@ random.intervals <- function (n, M, unique.int = F){
 #' A function performing seeded binary segmentation. 
 #' It is slightly modifying the "wbs" function from the "wbs" package (version 1.3) to use seeded intervals.
 #'
-#' @param decay a decay (for seeded intervals) used in the Seeded Binary Segmentation algorithm
+#' @param decay a decay for seeded intervals (1/a in the notation of the paper, to be precise) used in the Seeded Binary Segmentation algorithm 
 #' @param ... not in use
 #' @return an object of class "wbs", which contains the following fields
 #' \item{x}{the input vector provided}
@@ -196,10 +196,10 @@ seedBS <- function (x, decay = sqrt(2), ...){
 # an example
 set.seed(1)
 X		<- rep(rep(0:1, each = 100), 5) + rnorm(1000)
-res_seedBS	<- seedBS(X)
-res_WBS		<- wbs(X)
-changepoints(res_seedBS)$cpt.ic[[2]]
-changepoints(res_WBS)$cpt.ic[[2]]
-# in this example the change point estimates are the same for seeded and wild binary segmentation, but the order
+results_seedBS	<- seedBS(X)
+results_wbs	<- wbs(X)
+changepoints(results_seedBS)$cpt.ic[[2]]
+changepoints(results_wbs)$cpt.ic[[2]]
+# In this example the change point estimates are the same for seeded and wild binary segmentation, but the order
 # of detection is apparently somewhat different.
 
